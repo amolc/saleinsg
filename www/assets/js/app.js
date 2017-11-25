592fba0e4896463e7b1ba4820255d03e1e49e0bd
Nadeem <nadyshaikh@gmail.com>
2017-11-25 19:08:36 +0530

    API URL changed

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -2,12 +2,12 @@
 var app = angular.module('80consult', ['angular-storage']);
 if (document.location.hostname == "saleinsg.com")
 {
-  var baseurl = "https://saleinsg/api/";
+  var baseurl = "https://saleinsg.com/api/";
 
 }
 else if (document.location.hostname == "www.saleinsg.com")
 {
-  var baseurl = "https://www.saleinsg/api/";
+  var baseurl = "https://www.saleinsg.com/api/";
 
 }else{
 
