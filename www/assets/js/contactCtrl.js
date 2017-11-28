b822b59937722386872c3487f548f6130d0b2f69
Unknown <komal@magnusideas.com>
2017-11-28 09:58:59 +0530

    change in order

--- www/assets/js/contactCtrl.js
+++ www/assets/js/contactCtrl.js
@@ -11,6 +11,8 @@
 
 app.controller('contactcontroller', function ($scope, $location, $http, $window) {
 
+     $window.Stripe.setPublishableKey('pk_test_lTp89fhcIMVEFL2HSVRqJTHO');
+
   //----------- Book Now ------------------------///
 
   $scope.adults = ['0','1','2','3','4','5','6','7','8','9','10'];
