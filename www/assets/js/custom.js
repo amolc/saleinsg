6d60dca942470d6d54d5c04badbed82e48d79a5e
Pravin Shelar <pravinshelar999@gmail.com>
2017-11-30 13:35:24 +0530

    all changes

--- www/assets/js/custom.js
+++ www/assets/js/custom.js
@@ -57,15 +57,3 @@ w3IncludeHTML();
 };
 /* ]]> */
 
-
-$(window).load(function(){
-    $("input").val("");
-    
-    $(".input-effect input").focusout(function(){
-      if($(this).val() != ""){
-        $(this).addClass("has-content");
-      }else{
-        $(this).removeClass("has-content");
-      }
-    })
-  });
