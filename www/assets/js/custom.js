7e38b324cff197be269565c2c099c06ed1927804
Pravin Shelar <pravinshelar999@gmail.com>
2017-12-25 17:28:49 +0530

    all pages

--- www/assets/js/custom.js
+++ www/assets/js/custom.js
@@ -26,7 +26,7 @@ $(function(){
   $('.carousel-showmanymoveone .item').each(function(){
     var itemToClone = $(this);
 
-    for (var i=1;i<4;i++) {
+    for (var i=1;i<5;i++) {
       itemToClone = itemToClone.next();
 
       // wrap around if at end of item collection
