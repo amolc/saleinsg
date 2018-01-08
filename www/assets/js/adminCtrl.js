c566b50ea5b5d52e0def9622b6e0ade268b4480f
Unknown <komal@magnusideas.com>
2018-01-08 18:07:29 +0530

    change in list

--- www/assets/js/adminCtrl.js
+++ www/assets/js/adminCtrl.js
@@ -68,12 +68,61 @@ app.controller('admincontroller', function ($scope, $location, $http, $window) {
           location.href = "index.html"
     }  
 
+     $scope.table = function(){
+
+       
+                        $('.dataTables-example').DataTable({
+                pageLength: 25,
+                responsive: true,
+                dom: '<"html5buttons"B>lTfgitp',
+                buttons: [
+                    { extend: 'copy'},
+                    {extend: 'csv'},
+                    {extend: 'excel', title: 'ExampleFile'},
+                    {extend: 'pdf', title: 'ExampleFile'},
+
+                    {extend: 'print',
+                     customize: function (win){
+                            $(win.document.body).addClass('white-bg');
+                            $(win.document.body).css('font-size', '10px');
+
+                            $(win.document.body).find('table')
+                                    .addClass('compact')
+                                    .css('font-size', 'inherit');
+                    }
+                    }
+                ]
+
+            });
+            $('[data-toggle="tooltip"]').tooltip();
+
+    }
+
     $scope.getAllUsers = function (req, res) {
 
 
             $http.get(baseurl + 'getAllUsers').success(function(data, status) {    
-                 // console.log(data); 
-                   $scope.userlist = data;  
+                   //console.log(status); 
+
+                   if (status == 200) 
+                   {
+
+                     $scope.userlist = data; 
+
+                      $.each($scope.userlist,function (index, value) { 
+
+                        SrNo = parseInt(index)+1;
+
+                        var content = '<tr><td>'+SrNo+'</td><td>'+value.FirstName+'</td><td>'+value.FirstName+'</td><td>'+value.CompanyName+'</td><td>'+value.Email+'</td><td>'+value.Phone+'</td><td>'+value.Location+'</td><td>'+value.CountryTitle+'</td></tr>';
+
+                        $("#usertable").append(content);
+                      
+                  }); 
+
+                      $scope.table();
+
+                   }
+                  
 
             });
 
@@ -90,6 +139,19 @@ app.controller('admincontroller', function ($scope, $location, $http, $window) {
             else {
                // console.log(res);
                 $scope.reqlist = res;
+
+                 $.each($scope.reqlist,function (index, value) { 
+
+                        SrNo = parseInt(index)+1;
+
+                        var content = '<tr><td>'+SrNo+'</td><td>'+value.FirstName+' '+value.LastName+'</td><td>'+value.CountryTitle+'</td><td>'+value.ProductName+'</td><td>'+value.Currency+' '+value.ExpectedPrice+'</td><td>'+value.Quantity+'</td><td>'+value.count+'</td></tr>';
+
+                        $("#requesttable").append(content);
+                      
+                  }); 
+
+                      $scope.table();
+
             }
 
         }).error(function () {
