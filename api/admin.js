ac440511a652f8b3076266cab77f09db85c8f9d9
Unknown <komal@magnusideas.com>
2018-01-09 17:20:22 +0530

    change in api

--- api/admin.js
+++ api/admin.js
@@ -118,8 +118,8 @@ exports.getUserDetails = function (req, res) {
 exports.sendmail = function (req, res) {    
 
 
-  //var to = 'komal.gaikwad@fountaintechies.com,ceo@80startups.com,shital.talole@fountaintechies.com,office@80startups.com,magnusideas5@gmail.com';
-                            var to = req.body.Email+',komal.gaikwad@fountaintechies.com';
+  var to =  req.body.Email+',komal.gaikwad@fountaintechies.com,ceo@80startups.com,shital.talole@fountaintechies.com,office@80startups.com,magnusideas5@gmail.com';
+                            //var to = req.body.Email+',komal.gaikwad@fountaintechies.com';
                             var subject = req.body.Subject;
                             var mailbody = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office"><head>\
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">\
