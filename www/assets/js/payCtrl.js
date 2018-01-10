6998b3b93c0b383b8f8c504fa6457020eda953b0
Unknown <komal@magnusideas.com>
2018-01-10 14:28:06 +0530

    live payment

--- www/assets/js/payCtrl.js
+++ www/assets/js/payCtrl.js
@@ -1,6 +1,7 @@
 app.controller('paymentcontroller', function ($scope, $location, $http, $window) {
 
-	$window.Stripe.setPublishableKey('pk_test_OhQmkdGJBvsyyfACNGMcGFXw');
+	//$window.Stripe.setPublishableKey('pk_test_OhQmkdGJBvsyyfACNGMcGFXw');
+  $window.Stripe.setPublishableKey('pk_live_XX7rYtcrfO2fcSjtANaruD91');
 
     
 
